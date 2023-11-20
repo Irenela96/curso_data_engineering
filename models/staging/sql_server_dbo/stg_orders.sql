601f8b8a7e342676b893c31fc6cef20ae9b61f24
Irene Lopez <bastetlanuit@gmail.com>
2023-11-20 19:01:45 +0000

    test de promo y order_id pasa

--- models/staging/sql_server_dbo/stg_orders.sql
+++ models/staging/sql_server_dbo/stg_orders.sql
@@ -10,14 +10,18 @@ with
             cast(shipping_cost as float) as shipping_cost,
             cast(address_id as varchar(50)) as address_id,
             cast(created_at as timestamp_ntz) as created_at,
-            cast(decode(promo_id, '', 'na', promo_id) as varchar(50)) as promo_id,
+            cast(
+                decode(
+                    promo_id, '', '9999', {{ dbt_utils.surrogate_key(["promo_id"]) }}
+                ) as varchar(50)
+            ) as promo_id,
             cast(estimated_delivery_at as timestamp_ntz) as estimated_delivery_at,
             cast(order_cost as float) as order_cost,
             cast(user_id as varchar(50)) as user_id,
             cast(decode(order_total, null, 0, order_total) as float) as order_total,
             cast(delivered_at as timestamp_ntz) as delivered_at,
             cast(tracking_id as varchar(50)) as tracking_id,
-            cast(decode(status, null, 'na', status) as varchar(20)) as status,
+            cast(decode(status, null, '9999', status) as varchar(20)) as status,
             cast(_fivetran_synced as timestamp_ntz) as date_load
         from src_orders
     )
--- models/staging/sql_server_dbo/stg_promos.sql
+++ models/staging/sql_server_dbo/stg_promos.sql
@@ -2,12 +2,12 @@
 
 with
     src_promos as (select * from {{ source("sql_server_dbo", "promos") }}),
-
     renamed_casted as (
         select
             cast(
-                decode(promo_id, '', 'Sin promo', promo_id) as varchar(50)
+                {{ dbt_utils.surrogate_key(["promo_id"]) }} as varchar(50)
             ) as promo_id,
+            cast(promo_id as varchar(50)) as des_promo,
             cast(discount as float) as discount,
             cast(status as varchar(50)) as status,
             cast(_fivetran_synced as timestamp_ntz) as date_load
@@ -16,3 +16,10 @@ with
 
 select *
 from renamed_casted
+union all
+select
+    '9999' as promo_id,
+    'Sin promo' as des_promo,
+    '0' as discount,
+    'inactive' as status,
+    {{ dbt_date.now("America/New_York") }} as date_load
