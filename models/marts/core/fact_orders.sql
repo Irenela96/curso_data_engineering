7c27b2fbf3fe42a3092aa6e3ea383ed982a613ee
Irene Lopez <bastetlanuit@gmail.com>
2023-11-23 16:53:33 +0000

    creando env de producción. Cambiando un macro para generar el nombre del esquema. Reescribiendo orders al original

--- models/marts/core/fact_orders.sql
+++ models/marts/core/fact_orders.sql
@@ -2,7 +2,7 @@
 
 with
     stag_orders as (select * from {{ ref("stg_orders") }}),
-    stag_order_items as (select* from {{ ref("stg_order_items")}}),
+
     cte1 as (
         select
             -- ids
@@ -13,7 +13,6 @@ with
             promo_id,
             status_id,
             tracking_id,
-            oi.order_items_id,
             -- dollars
             order_cost_dollars,
             order_total_dollars,
@@ -24,8 +23,6 @@ with
             o.date_load_utc as date_load_orders_utc,
             oi.date_load_utc as date_load_order_items_utc
         from stag_orders o
-        join stag_order_items oi on o.order_id = oi.order_id
-        order by order_id
     )
 
 select *
