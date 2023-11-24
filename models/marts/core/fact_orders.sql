59e82f2db6f03d39f953ff6e87abb5efa51683d5
Irene Lopez <bastetlanuit@gmail.com>
2023-11-24 09:09:54 +0000

    + events_id a la tabla de hechos, + order_total en stg_users, cambiando alias fechas en orders para tener todas las fechas con _utc

--- models/marts/core/fact_orders.sql
+++ models/marts/core/fact_orders.sql
@@ -1,7 +1,31 @@
 {{ config(materialized="view") }}
 
 with
-    stag_orders as (select * from {{ ref("stg_orders") }})
+    stag_orders as (select * from {{ ref("stg_orders") }}),
+    stag_events as (select order_id, event_id from {{ ref("stg_events") }}),
+    cte as (
+        select
+            o.order_id,
+            o.user_id,
+            address_id,
+            promo_id,
+            shipping_service_id,
+            tracking_id,
+            o.created_at_utc,
+            status_id,
+            e.event_id,
+            oi.order_items,
+            order_cost_dollars,
+            order_total_dollars,
+            delivered_at_utc,
+            estimated_delivery_at_utc,
+            created_at_utc,
+            date_load_utc
+        from stag_orders o
+        join stag_events e on o.order_id = e.order_id
+        join stag_order_items oi on o.order_id = oi.order_id 
+    )
+
 
 select *
-from stag_orders
+from cte
