45adc947ce3cfafc7d146370acffb5b664136fd8
Irene Lopez <bastetlanuit@gmail.com>
2023-11-26 09:53:43 +0000

    + numero de orders y total de orders

--- models/marts/core/fact_orders.sql
+++ models/marts/core/fact_orders.sql
@@ -7,41 +7,52 @@
 }}
 
 with
-    stag_orders as (select * from {{ ref("stg_orders") }}),
-    stag_events as (select order_id, event_id from {{ ref("stg_events") }}
-    where event_type = 'package_shipped'),
-    cte as (
-        select
-            concat(
-                o.order_id,
-                user_id,
-                address_id,
-                promo_id,
-                shipping_service_id,
-                tracking_id,
-                status_id,
-                e.event_id
-            ) as order_id,
-            o.order_id as order_items_id,
-            o.user_id,
+stag_orders as (select * from {{ ref("stg_orders") }}),
+
+stag_events as (
+    select
+        order_id,
+        event_id
+    from {{ ref("stg_events") }}
+    where event_type = 'package_shipped'
+),
+
+cte as (
+    select
+        o.order_id as order_items_id,
+        o.user_id,
+        address_id,
+        promo_id,
+        shipping_service_id,
+        tracking_id,
+        o.created_at_utc,
+        status_id,
+        e.event_id,
+        shipping_cost_dollars,
+        order_cost_dollars,
+        order_total_dollars,
+        num_orders_user,
+        total_num_orders_user,
+        delivered_at_utc,
+        estimated_delivery_at_utc,
+        order_time_days, 
+        date_load_utc,
+        concat(
+            o.order_id,
+            user_id,
             address_id,
             promo_id,
             shipping_service_id,
             tracking_id,
-            o.created_at_utc,
             status_id,
-            e.event_id,
-            shipping_cost_dollars,
-            order_cost_dollars,
-            order_total_dollars,
-            delivered_at_utc,
-            estimated_delivery_at_utc,
-            date_load_utc
-        from stag_orders o
-        join stag_events e on o.order_id = e.order_id
-    ),
-    cte1 as (
-        select
+            e.event_id
+        ) as order_id
+    from stag_orders as o
+    inner join stag_events as e on o.order_id = e.order_id
+),
+
+cte1 as (
+    select
             {{ dbt_utils.generate_surrogate_key(["order_id"]) }} as order_id,
             order_items_id,
             user_id,
@@ -54,13 +65,15 @@ with
             shipping_cost_dollars,
             order_cost_dollars,
             order_total_dollars,
+            num_orders_user,
+            total_num_orders_user,
             created_at_utc,
             estimated_delivery_at_utc,
             delivered_at_utc,
-            round(datediff(day,created_at_utc,delivered_at_utc)) order_time_days,    
+            order_time_days,    
             date_load_utc
-        from cte
-    )
+from cte
+)
 
 select *
 from cte1
--- models/staging/sql_server_dbo/stg_orders.sql
+++ models/staging/sql_server_dbo/stg_orders.sql
@@ -16,9 +16,12 @@ with
             cast(shipping_cost as float) as shipping_cost_dollars,
             cast(order_cost as float) as order_cost_dollars,
             cast(coalesce(order_total, 0) as float) as order_total_dollars,
+            row_number() over (partition by user_id order by created_at) num_orders_user,
+            count(order_id) over (partition by user_id) total_num_orders_user,
             convert_timezone('UTC', created_at) as created_at_utc,
             convert_timezone('UTC', delivered_at) as delivered_at_utc,
-            convert_timezone('UTC', estimated_delivery_at) as estimated_delivery_at_utc,            
+            convert_timezone('UTC', estimated_delivery_at) as estimated_delivery_at_utc,
+            round(datediff(day,created_at_utc,delivered_at_utc)) order_time_days,          
             convert_timezone('UTC', _fivetran_synced) as date_load_utc
         from src_orders
     )
